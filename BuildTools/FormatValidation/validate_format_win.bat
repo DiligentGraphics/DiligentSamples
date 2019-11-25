6342cadd8a5137231a56e1f05d95dcc228ac8429
Egor Yusov <egor.yusov@gmail.com>
2019-11-25 09:46:30 -0800

    Added format validation .bat file and CMake target

---
+++ BuildTools/FormatValidation/validate_format_win.bat
@@ -0,0 +1,5 @@
+python ../../../DiligentCore/BuildTools/FormatValidation/clang-format-validate.py --color never ^
+--clang-format-executable ../../../DiligentCore/BuildTools/FormatValidation/clang-format_10.0.0.exe ^
+-r ../../SampleBase ^
+--exclude ../../SampleBase/src/UWP ^
+--exclude ../../SampleBase/src/Win32/resources
