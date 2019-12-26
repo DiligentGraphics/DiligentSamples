a8c99db783ae9e84469fbce45e85d3e2012dd165
assiduous <assiduous@diligentgraphics.com>
2019-12-26 13:56:04 -0800

    Fixed artifacts path

--- BuildTools/Scripts/appveyor/run_tests.bat
+++ BuildTools/Scripts/appveyor/run_tests.bat
@@ -1,9 +1,9 @@
 @echo off
 
 if "%PLATFORM_NAME%"=="Windows" (
-    ProcessGoldenImages.bat %1 %CONFIGURATION% %2 d3d11 d3d12
+    call ProcessGoldenImages.bat %1 %CONFIGURATION% %2 d3d11 d3d12
 )
 
 if "%PLATFORM_NAME%"=="Windows8.1" (
-    ProcessGoldenImages.bat %1 %CONFIGURATION% %2 d3d11
+    call ProcessGoldenImages.bat %1 %CONFIGURATION% %2 d3d11
 )
