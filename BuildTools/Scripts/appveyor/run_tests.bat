ed474a7f70d35c870e6f7ac95cb0318a1fe21e76
assiduous <assiduous@diligentgraphics.com>
2019-12-26 12:31:13 -0800

    Appveyor: updated script to generate golden images

--- BuildTools/Scripts/appveyor/run_tests.bat
+++ BuildTools/Scripts/appveyor/run_tests.bat
@@ -4,11 +4,11 @@ setlocal ENABLEDELAYEDEXPANSION
 set ERROR=0
 
 if "%PLATFORM_NAME%"=="Windows" (
-    call ProcessGoldenImages.bat %1 %CONFIGURATION% compare d3d11 d3d12 || set ERROR=!ERRORLEVEL!
+    call ProcessGoldenImages.bat %1 %CONFIGURATION% %2 d3d11 d3d12 || set ERROR=!ERRORLEVEL!
 )
 
 if "%PLATFORM_NAME%"=="Windows8.1" (
-    call ProcessGoldenImages.bat %1 %CONFIGURATION% compare d3d11 || set ERROR=!ERRORLEVEL!
+    call ProcessGoldenImages.bat %1 %CONFIGURATION% %2 d3d11 || set ERROR=!ERRORLEVEL!
 )
 
 exit /B %ERROR%
