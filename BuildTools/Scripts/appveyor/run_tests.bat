347e4bab75196c10c822251cfe7b74e429f07aa3
assiduous <assiduous@diligentgraphics.com>
2019-12-26 14:42:21 -0800

    Updated run_tests.bat; temporarily disabled vulkan

--- BuildTools/Scripts/appveyor/run_tests.bat
+++ BuildTools/Scripts/appveyor/run_tests.bat
@@ -1,9 +1,9 @@
 @echo off
 
 if "%PLATFORM_NAME%"=="Windows" (
-    call ProcessGoldenImages.bat %1 %CONFIGURATION% %2 d3d11 d3d12
+    ProcessGoldenImages.bat %1 %CONFIGURATION% %2 d3d11 d3d12
 )
 
 if "%PLATFORM_NAME%"=="Windows8.1" (
-    call ProcessGoldenImages.bat %1 %CONFIGURATION% %2 d3d11
+    ProcessGoldenImages.bat %1 %CONFIGURATION% %2 d3d11
 )
