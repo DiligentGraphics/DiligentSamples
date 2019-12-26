a9572742fcf4b96ddba227f9d37c6f16bbf3f0ba
assiduous <assiduous@diligentgraphics.com>
2019-12-26 13:28:37 -0800

    Updated run_tests.bat to not use call

--- BuildTools/Scripts/appveyor/run_tests.bat
+++ BuildTools/Scripts/appveyor/run_tests.bat
@@ -1,14 +1,9 @@
 @echo off
-setlocal ENABLEDELAYEDEXPANSION
-
-set ERROR=0
 
 if "%PLATFORM_NAME%"=="Windows" (
-    call ProcessGoldenImages.bat %1 %CONFIGURATION% %2 d3d11 d3d12 || set ERROR=!ERRORLEVEL!
+    ProcessGoldenImages.bat %1 %CONFIGURATION% %2 d3d11 d3d12
 )
 
 if "%PLATFORM_NAME%"=="Windows8.1" (
-    call ProcessGoldenImages.bat %1 %CONFIGURATION% %2 d3d11 || set ERROR=!ERRORLEVEL!
+    ProcessGoldenImages.bat %1 %CONFIGURATION% %2 d3d11
 )
-
-exit /B %ERROR%
