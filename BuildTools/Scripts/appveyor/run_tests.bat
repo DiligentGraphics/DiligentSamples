598990f799da4c19568fb308b551ba3cd69ecdcf
assiduous <assiduous@diligentgraphics.com>
2019-12-26 09:44:58 -0800

    Updated run_tests.bat in an attempt to see output in the buid log

--- BuildTools/Scripts/appveyor/run_tests.bat
+++ BuildTools/Scripts/appveyor/run_tests.bat
@@ -1,10 +1,15 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+
+set ERROR=0
+
 if "%PLATFORM_NAME%"=="Windows" (
-    rem  Note: 'exit /b' command in the bat file will terminate this shell too 
-	rem  (which is what we want in this case).
-	rem  We can run the script with 'call' to keep this shell running.
-    ProcessGoldenImages.bat %1 %CONFIGURATION% compare d3d11 d3d12
+    :: For some reason if the script is run without 'call', no output is displayed in the build log
+    call ProcessGoldenImages.bat %1 %CONFIGURATION% compare d3d11 d3d12 || set ERROR=!ERRORLEVEL!
 )
 
 if "%PLATFORM_NAME%"=="Windows8.1" (
-    ProcessGoldenImages.bat %1 %CONFIGURATION% compare d3d11
+    call ProcessGoldenImages.bat %1 %CONFIGURATION% compare d3d11 || set ERROR=!ERRORLEVEL!
 )
+
+exit /B %ERROR%
