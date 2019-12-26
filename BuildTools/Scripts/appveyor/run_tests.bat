b8d6167bd1a9f2f5b394dd36994313a85c8c4808
assiduous <assiduous@diligentgraphics.com>
2019-12-26 09:51:12 -0800

    Updated appveyor script to define platfrom_name variable

--- BuildTools/Scripts/appveyor/run_tests.bat
+++ BuildTools/Scripts/appveyor/run_tests.bat
@@ -4,7 +4,6 @@ setlocal ENABLEDELAYEDEXPANSION
 set ERROR=0
 
 if "%PLATFORM_NAME%"=="Windows" (
-    :: For some reason if the script is run without 'call', no output is displayed in the build log
     call ProcessGoldenImages.bat %1 %CONFIGURATION% compare d3d11 d3d12 || set ERROR=!ERRORLEVEL!
 )
 
