f6a30456477da94aa330dcb76ee8b0671800afff
assiduous <assiduous@diligentgraphics.com>
2019-12-26 21:36:01 -0800

    run_tests.bat: do not run tests in d3d12 mode on 32-bit windows due to bugs in 32-bit WARP

--- BuildTools/Scripts/appveyor/run_tests.bat
+++ BuildTools/Scripts/appveyor/run_tests.bat
@@ -1,7 +1,13 @@
 @echo off
 
 if "%PLATFORM_NAME%"=="Windows" (
-    ProcessGoldenImages.bat %1 %CONFIGURATION% %2 d3d11 d3d12
+    if "%PLATFORM%" == "x64" (
+        ProcessGoldenImages.bat %1 %CONFIGURATION% %2 d3d11 d3d12
+    ) else (
+        rem  There appear to be bugs in 32-bit D3D12 WARP that cause shader compilation errors in
+        rem  Tutorial14 and Atmosphere sample, so only run tests in d3d11 mode
+        ProcessGoldenImages.bat %1 %CONFIGURATION% %2 d3d11
+    )
 )
 
 if "%PLATFORM_NAME%"=="Windows8.1" (
