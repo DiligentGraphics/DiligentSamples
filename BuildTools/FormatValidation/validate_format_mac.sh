8f1148185b7215e0ae72fc235c0a3bd17e4edeeb
assiduous <assiduous@diligentgraphics.com>
2020-03-26 15:08:25 -0700

    Enabled format validation on Mac

--- BuildTools/FormatValidation/validate_format_mac.sh
+++ BuildTools/FormatValidation/validate_format_mac.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "Format validation is not yet enabled for DiligentSamples module on MacOS"
-
+python ../../../DiligentCore/BuildTools/FormatValidation/clang-format-validate.py \
+--clang-format-executable ../../../DiligentCore/BuildTools/FormatValidation/clang-format_10.0.0 \
+-r ../../SampleBase ../../Tutorials ../../Samples \
+--exclude ../../SampleBase/src/UWP \
+--exclude ../../SampleBase/src/Win32/resources
