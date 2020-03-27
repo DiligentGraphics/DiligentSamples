d560a88b6780f8bf35a72b1661723cd9c93eb1d1
assiduous <assiduous@diligentgraphics.com>
2020-03-26 19:56:40 -0700

    Enabled format validation on Linux

--- BuildTools/FormatValidation/validate_format_linux.sh
+++ BuildTools/FormatValidation/validate_format_linux.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo "Format validation is not yet enabled for DiligentSamples module on Linux"
-
+python ../../../DiligentCore/BuildTools/FormatValidation/clang-format-validate.py \
+--clang-format-executable ../../../DiligentCore/BuildTools/FormatValidation/clang-format_linux_10.0.0 \
+-r ../../SampleBase ../../Tutorials ../../Samples \
+--exclude ../../SampleBase/src/UWP \
+--exclude ../../SampleBase/src/Win32/resources
