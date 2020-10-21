76409e251eb25674cb0addb838ceed26bdc83d15
assiduous <assiduous@diligentgraphics.com>
2020-10-20 20:44:12 -0700

    Updated ProcessGoldenImages.bat to allow passing custom arguments

--- Tests/GoldenImages/ProcessGoldenImages.bat
+++ Tests/GoldenImages/ProcessGoldenImages.bat
@@ -3,19 +3,18 @@
 set num_args=0
 for %%x in (%*) do Set /A num_args+=1
 
-if "%num_args%" LSS "5" (
+if "%num_args%" LSS "4" (
     echo Command line format:
     echo.
-    echo   ProcessGoldenImages.bat build_path config mode extra_args backends
+    echo   ProcessGoldenImages.bat build_path config golden_img_mode test_modes
     echo.
-    echo build_path - path to the root of the build tree
-    echo config     - configuration (Debug, Release, etc.^)
-    echo mode       - golden image processing mode (capture or compare^)
-    echo extra_args - extra arguments for command line (e.g. "-adater sw"^)
-    echo backends   - list of rendering backends (e.g. d3d11 d3d12 gl vk^)
+    echo build_path      - path to the root of the build tree
+    echo config          - configuration (Debug, Release, etc.^)
+    echo golden_img_mode - golden image processing mode (capture or compare^)
+    echo test_modes      - list of test modes (e.g. "-mode d3d11" "-mode d3d11 -adapter sw" "-mode d3d12" "-mode gl" "-mode vk"^)
     echo. 
     echo Example:
-    echo   ProcessGoldenImages.bat c:\Projects\DiligentEngine\build\Win64 Debug compare "-adapter sw" d3d11 d3d12
+    echo   ProcessGoldenImages.bat c:\Projects\DiligentEngine\build\Win64 Debug compare "-mode d3d11" "-mode d3d12 -adapter sw"
     EXIT /B -1
 )
 
@@ -33,6 +32,7 @@ if "%golden_images_root_dir%" == "" (
     set golden_images_root_dir=../../../Tests/GoldenImages
 )
 
+ :: ~ removes surrounding quotes
 set build_folder=%~1
 shift
 
@@ -42,12 +42,10 @@ shift
 set golden_img_mode=%1
 shift
 
-set extra_agrs=%~1  :: ~ removes surrounding quotes
-shift
-
 set rest_args=
 :loop1
-    if "%1"=="" goto end_loop1
+    if "%~1"=="" goto end_loop1
+    :: Do not remove quotes yet as this will ungroup arguments
     set rest_args=%rest_args% %1
     shift
     goto loop1
@@ -120,10 +118,11 @@ EXIT /B %ERROR%
     )
 	if "%app_name%" == "Tutorial18_Queries" set show_ui=0
 	
-    set backends=
+    set test_modes=
     :loop2
-        if "%1"=="" goto end_loop2
-        set backends=%backends% %1
+	    :: Do not remove quotes as this will ungroup arguments
+        if "%~1"=="" goto end_loop2
+        set test_modes=%test_modes% %1
         shift
         goto loop2
     :end_loop2
@@ -136,30 +135,48 @@ EXIT /B %ERROR%
     )
 
     set EXIT_CODE=0
-    for %%X in (%backends%) do (
+    for %%X in (%test_modes%) do (
 
         set app_path=%build_folder%/DiligentSamples/%app_folder%/%app_name%/%config%/%app_name%.exe
-        set capture_name=%app_name%_gi_%%X
+
+        :: Get the backend name and extra arguments
+        set extra_args=
+        set mode_param=0
+        for %%Y in (%%~X) do (
+            if "%%Y" == "-mode" (
+                set mode_param=1
+            ) else (
+                if "!mode_param!" == "1" (
+                    set backend_name=%%Y
+                    set mode_param=0
+                ) else (
+                    set extra_args=!extra_args! %%Y
+                )
+            )
+        )
+        set capture_name=%app_name%_gi_!backend_name!
+        
 
         rem   !!!   ERRORLEVEL doesn't get updated inside control blocks like IF statements unless           !!!
         rem   !!!   !ERRORLEVEL! is used instead of %ERRORLEVEL% and delayed expansion is enabled as below:  !!!
         rem   !!!   setlocal ENABLEDELAYEDEXPANSION                                                          !!!
-        !app_path! -mode %%X %extra_agrs% -width %golden_img_width% -height %golden_img_height% -golden_image_mode %golden_img_mode% -capture_path %golden_img_dir% -capture_name !capture_name! -capture_format png -adapters_dialog 0 -show_ui %show_ui%
+        rem   ~ removes quotes from %%~X
+        !app_path! %%~X -width %golden_img_width% -height %golden_img_height% -golden_image_mode %golden_img_mode% -capture_path %golden_img_dir% -capture_name !capture_name! -capture_format png -adapters_dialog 0 -show_ui %show_ui%
 
         if !ERRORLEVEL! NEQ 0 (
             if !ERRORLEVEL! GEQ 0 (
-                echo Golden image validation failed for %app_name% [%%X]: !ERRORLEVEL! incosistent pixels found
+                echo Golden image validation FAILED for %app_name% [!backend_name!!extra_args!]: !ERRORLEVEL! incosistent pixels found
                 if "%golden_img_mode%" == "compare_update" echo Golden image updated
             ) else (
-                if "%golden_img_mode%" == "compare" echo Golden image validation failed for %app_name% [%%X]
-                if "%golden_img_mode%" == "capture" echo Failed to generate golden image for %app_name% [%%X]
-                if "%golden_img_mode%" == "compare_update" echo Failed to validate or update golden image for %app_name% [%%X]
+                if "%golden_img_mode%" == "compare" echo Golden image validation FAILED for %app_name% [!backend_name!!extra_args!]: unknown error
+                if "%golden_img_mode%" == "capture" echo FAILED to generate golden image for %app_name% [!backend_name!!extra_args!]: unknown error
+                if "%golden_img_mode%" == "compare_update" echo FAILED to validate or update golden image for %app_name% [!backend_name!!extra_args!]: unknown error
             )
             set EXIT_CODE=1
         ) else (
-            if "%golden_img_mode%" == "compare" echo Golden image validation passed for %app_name% [%%X]
-            if "%golden_img_mode%" == "capture" echo Successfully generated golden image for %app_name% [%%X]
-            if "%golden_img_mode%" == "compare_update" echo Golden image validation passed for %app_name%. Image updated. [%%X]
+            if "%golden_img_mode%" == "compare" echo Golden image validation PASSED for %app_name% [!backend_name!!extra_args!]
+            if "%golden_img_mode%" == "capture" echo Successfully generated golden image for %app_name% [!backend_name!!extra_args!]
+            if "%golden_img_mode%" == "compare_update" echo Golden image validation PASSED for %app_name%. Image updated. [!backend_name!!extra_args!]
         )
 
 		echo.
