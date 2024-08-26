2e2098ba833c624477c5dc0fe4872c1b862a8afe
Mikhail Gorobets <mickgorobets@gmail.com>
2024-08-27 05:22:00 +0600

    Web: Imporved WASM asset packing (close #210)

--- BuildTools/Emscripten/pack-artifacts.py
+++ BuildTools/Emscripten/pack-artifacts.py
@@ -29,6 +29,8 @@ BUILD_TARGETS = [
     "GLTFViewer"
 ]
 
+EXCLUDE_ITEMS = ["CMakeFiles", "cmake_install.cmake"]
+
 def pack_artifacts(source_dir, destination_dir):
     if not os.path.exists(destination_dir):
         os.makedirs(destination_dir)
@@ -37,21 +39,34 @@ def pack_artifacts(source_dir, destination_dir):
         source_folder_path_tutorials = os.path.join(source_dir, "Tutorials", folder)
         source_folder_path_samples = os.path.join(source_dir, "Samples", folder)
         destination_folder_path = os.path.join(destination_dir, folder)
-       
+
         if os.path.exists(source_folder_path_tutorials):
-            shutil.copytree(source_folder_path_tutorials, destination_folder_path, dirs_exist_ok=True)
+            copy_filtered(source_folder_path_tutorials, destination_folder_path)
         elif os.path.exists(source_folder_path_samples):
-            shutil.copytree(source_folder_path_samples, destination_folder_path, dirs_exist_ok=True)
+            copy_filtered(source_folder_path_samples, destination_folder_path)
         else:
             print(f"Folder '{folder}' not found.")
 
+def copy_filtered(source, destination):
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+
+    for item in os.listdir(source):
+        src_path = os.path.join(source, item)
+        dest_path = os.path.join(destination, item)
+
+        if item in EXCLUDE_ITEMS:
+            continue
+
+        if os.path.isdir(src_path):
+            shutil.copytree(src_path, dest_path, dirs_exist_ok=True)
+        else:
+            shutil.copy2(src_path, dest_path)
+
 if __name__ == '__main__':
     parser = ArgumentParser("Pack WASM modules")
-    parser.add_argument("-s", "--source_dir",
-                        required=True)
-
-    parser.add_argument("-o", "--output_dir",
-                        required=True)
+    parser.add_argument("-s", "--source_dir", required=True)
+    parser.add_argument("-o", "--output_dir", required=True)
 
     args = parser.parse_args()
     pack_artifacts(args.source_dir, args.output_dir)
