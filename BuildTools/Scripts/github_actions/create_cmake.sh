ffcaacca4c7538465b851908b7db6b2e34c603aa
assiduous <assiduous@diligentgraphics.com>
2025-02-09 15:08:23 -0800

    Bump minimum CMake version to 3.10

--- BuildTools/Scripts/github_actions/create_cmake.sh
+++ BuildTools/Scripts/github_actions/create_cmake.sh
@@ -1,5 +1,5 @@
 cd $1
-echo "cmake_minimum_required(VERSION 3.6)" > CMakeLists.txt
+echo "cmake_minimum_required(VERSION 3.10)" > CMakeLists.txt
 echo "Project(DiligentSamples_Test)" >> CMakeLists.txt
 echo "add_subdirectory(DiligentCore)" >> CMakeLists.txt
 echo "add_subdirectory(DiligentTools)" >> CMakeLists.txt
